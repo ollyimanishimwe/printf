a0112454612d04574bddda320809464ca9af38e1
Christian <nshimyumukizachristian@gmail.com>
2021-03-19 23:17:43 +0000

    fix

--- _print_string.c
+++ _print_string.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include "unistd.h"
+
 /**
  * print_string - prints a string to stdout
  * @str: string to be printed
@@ -9,12 +9,8 @@ int print_string(char *str)
 {
 	int chars_printed = 0;
 
-	if (str == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-
+	if (str == (char *)0)
+		return (print_string("(null)"));
 	while (str && str[chars_printed])
 	{
 		_putchar(str[chars_printed]);
